54a66384b7157299d4c0dbce5fb47f3922fe676a
lamnhuthoa <hoalamnhut@gmail.com>
2023-04-29 18:27:13 +0700

    Complete basic state management

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,13 +1,35 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 
+const COUNTER_AMOUNT = 1;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + action.payload };
+    case "decrement":
+      return state.count === 0
+        ? state
+        : { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
   return (
     <View>
-      <Button title="Increase" onPress={() => setCount(count + 1)} />
-      <Button title="Decrease" onPress={() => count == 0 ? setCount(0) : setCount(count - 1)} />
-      <Text>Current Count: {count}</Text>
+      <Button
+        title="Increase"
+        onPress={() => dispatch({ type: "increment", payload: COUNTER_AMOUNT })}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => dispatch({ type: "decrement", payload: COUNTER_AMOUNT })}
+      />
+      <Text>Current Count: {state.count}</Text>
     </View>
   );
 };
---
+++ src/screens/TextScreen.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TextInput } from "react-native";
+
+const TextScreen = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View>
+      <Text>Enter Name:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
+      />
+      <Text>Your name is {name}</Text>
+      <br />
+      <br />
+      <Text>Enter Password:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
+      />
+      {password.length < 4 ? <Text>Password must be 5 characters</Text> : null}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
+
+export default TextScreen;
